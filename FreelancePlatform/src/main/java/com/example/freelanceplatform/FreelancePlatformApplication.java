5105806cd396cebbbc929983ff65cf5a9e6aa40f
beliadan <beliadan@fel.cvut.cz>
2024-04-06 14:53:34 +0200

    resetup project

--- FreelancePlatform/src/main/java/com/example/freelanceplatform/FreelancePlatformApplication.java
+++ FreelancePlatform/src/main/java/com/example/freelanceplatform/FreelancePlatformApplication.java
@@ -1,4 +1,4 @@
-package freelanceplatform;
+package com.example.freelanceplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FreelancePlatformApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FreelancePlatformApplication.class, args);
-
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancePlatformApplication.class, args);
+	}
 
 }
--- FreelancePlatform/src/test/java/com/example/freelanceplatform/FreelancePlatformApplicationTests.java
+++ FreelancePlatform/src/test/java/com/example/freelanceplatform/FreelancePlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package freelanceplatform;
+package com.example.freelanceplatform;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FreelancePlatformApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
